64395e68a83882c798c5f4b1ddd41625c197390c
Sandro Alan Gomez Aceituno <zandroallan@gmail.com>
2025-03-27 17:36:07 -0600

    Intregracion formulario marcos 27 de marzo de 2025

--- storage/framework/views/901064df69fa80bee34b9d3d57050611aafe3f5d.php
+++ storage/framework/views/901064df69fa80bee34b9d3d57050611aafe3f5d.php
@@ -42,13 +42,10 @@
 		</section>
 
 		<section id="content6">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<?php echo $__env->make('backend.mis-tramites.tabs-financiera', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 		</section>
 
-		<section id="content7"><?php echo $__env->make('backend.mis-tramites.tabs-contacto', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?></section>
+		<section id="content7">
+			<?php echo $__env->make('backend.mis-tramites.tabs-contacto', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+		</section>
 <?php /**PATH C:\AppServ\www\sircse\resources\views/backend/mis-tramites/tabs.blade.php ENDPATH**/ ?>
