bb4cd8b1618ee95498d2bf9e6c02714d13deee23
MarcoVillegas01 <158064005+MarcoVillegas01@users.noreply.github.com>
2025-04-05 19:03:44 -0600

    reemplazo de sweetalerts terminado

--- public/js/backend/general.js
+++ public/js/backend/general.js
@@ -41,7 +41,7 @@ function save(button) {
                 confirmar: {
                     text: 'Confirmar',
                     btnClass: 'btn btn-primary',
-                    action: function() {
+                    action: function () {
                         clicando = true;
                         $("#frm-1").submit();
                     }
@@ -49,7 +49,7 @@ function save(button) {
                 cancelar: {
                     text: 'Cancelar',
                     btnClass: 'btn btn-default',
-                    action: function() {
+                    action: function () {
                         clicando = false;
                     }
                 }
@@ -78,7 +78,7 @@ function save(button) {
         });
     }
 }
-$("#b-check-1").click(function() {
+$("#b-check-1").click(function () {
     buttonpressed = $(this).attr('name');
     // swal({
     //     title: "¡ Advertencia !",
@@ -101,19 +101,19 @@ $("#b-check-1").click(function() {
             confirmar: {
                 text: 'Confirmar',
                 btnClass: 'btn btn-primary',
-                action: function() {
+                action: function () {
                 }
             },
             cancelar: {
                 text: 'Cancelar',
                 btnClass: 'btn btn-default',
-                action: function() {
+                action: function () {
                 }
             }
         }
     });
 });
-$('#frm-1').on('submit', function(e) {
+$('#frm-1').on('submit', function (e) {
     var el = $('#frm-1');
     e.preventDefault();
     var str_errors;
@@ -123,27 +123,46 @@ $('#frm-1').on('submit', function(e) {
         data: new FormData(this),
         processData: false,
         contentType: false,
-        success: function(json) {
+        success: function (json) {
             messages_validation(json.data, false);
-            swal({
-                type: 'success',
-                title: 'Confirmación',
-                content: {
-                    element: 'p',
-                    attributes: {
-                        innerHTML: json.msg,
+            // swal({
+            //     type: 'success',
+            //     title: 'Confirmación',
+            //     content: {
+            //         element: 'p',
+            //         attributes: {
+            //             innerHTML: json.msg,
+            //         },
+            //     },
+            //     showConfirmButton: false,
+            //     timer: 1500
+            // }).then(function() {
+            //     clicando = false;
+            //     if (json.route_redirect != "") {
+            //         window.location = json.route_redirect;
+            //     }
+            // });
+            $.confirm({
+                title: 'Confirmacion',
+                type: 'green',
+                typeAnimated: 'true',
+                icon: 'fa fa-check',
+                content: json.msg,
+                autoClose: 'close|1500',
+                buttons: {
+                    close: {
+                        isHidden: true,
                     },
                 },
-                showConfirmButton: false,
-                timer: 1500
-            }).then(function() {
-                clicando = false;
-                if (json.route_redirect != "") {
-                    window.location = json.route_redirect;
+                onclose: function () {
+                    clicando = false;
+                    if (json.route_redirect != "") {
+                        window.location = json.route_redirect;
+                    }
                 }
             });
         },
-        error: function(json) {
+        error: function (json) {
             var jsonString = json.responseJSON;
             if (json.status === 422) {
                 messages_validation(null, false);
@@ -153,22 +172,38 @@ $('#frm-1').on('submit', function(e) {
             if (json.status === 409) {
                 str_errors = jsonString.msg;
             }
-            swal({
-                title: "¡ Advertencia !",
-                content: {
-                    element: 'p',
-                    attributes: {
-                        innerHTML: str_errors,
-                    },
-                },
-                icon: "warning",
+            // swal({
+            //     title: "¡ Advertencia !",
+            //     content: {
+            //         element: 'p',
+            //         attributes: {
+            //             innerHTML: str_errors,
+            //         },
+            //     },
+            //     icon: "warning",
+            //     buttons: {
+            //         confirm: {
+            //             text: 'Confirmar',
+            //             value: true,
+            //             visible: true,
+            //             className: 'btn btn-primary',
+            //             closeModal: true
+            //         }
+            //     }
+            // });
+            $.confirm({
+                title: '¡ Advertencia !',
+                content: str_errors,
+                icon: 'fa fa-warning',
+                type: 'orange',
+                typeAnimated: 'true',
                 buttons: {
                     confirm: {
                         text: 'Confirmar',
-                        value: true,
-                        visible: true,
-                        className: 'btn btn-primary',
-                        closeModal: true
+                        btnClass: 'btn btn-primary',
+                        action: function () {
+                            return true;
+                        }
                     }
                 }
             });
@@ -176,24 +211,46 @@ $('#frm-1').on('submit', function(e) {
         }
     });
 });
-$(".btn-eliminar").click(function() {
+$(".btn-eliminar").click(function () {
     buttonpressed = $(this).attr('name');
-    swal({
+    // swal({
+    //     title: '¡ Advertencia !',
+    //     text: "Esta seguro de eliminar los datos?",
+    //     type: 'warning',
+    //     showCancelButton: true,
+    //     confirmButtonColor: '#3085d6',
+    //     cancelButtonColor: '#d33',
+    //     confirmButtonText: 'Confirmar',
+    //     cancelButtonText: 'Cancelar'
+    // }).then((result) => {
+    //     if (result.value) {
+    //         $(".myformdelete").submit();
+    //     }
+    // });
+    $.confirm({
         title: '¡ Advertencia !',
-        text: "Esta seguro de eliminar los datos?",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Confirmar',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.value) {
-            $(".myformdelete").submit();
+        content: "¿Está seguro de eliminar los datos?",
+        type: 'orange',
+        typeAnimated: 'true',
+        icon: 'fa fa-warning',
+        buttons: {
+            confirmar: {
+                text: 'Confirmar',
+                btnClass: 'btn btn-primary',
+                action: function () {
+                    $(".myformdelete").submit();
+                }
+            },
+            cancelar: {
+                text: 'Cancelar',
+                btnClass: 'btn btn-default',
+                action: function () {
+                }
+            }
         }
     });
 });
-$('#myform').on('submit', function(e) {
+$('#myform').on('submit', function (e) {
     var l = $('.' + buttonpressed).ladda();
     l.ladda('start');
     var el = $('#myform');
@@ -204,70 +261,122 @@ $('#myform').on('submit', function(e) {
         data: new FormData(this),
         processData: false,
         contentType: false,
-        success: function(json) {
+        success: function (json) {
             if (json.origin == 'create') {
                 clean_form();
             }
             var fields = json.data;
             messages_validation(fields, false);
-            swal({
-                type: 'success',
+            // swal({
+            //     type: 'success',
+            //     title: 'Exito',
+            //     html: json.success,
+            //     showConfirmButton: false,
+            //     timer: 1500
+            // }).then(function () {
+            //     l.ladda('stop');
+            //     if (json.origin == 'update') {
+            //         window.location = json.url;
+            //     }
+            // });
+            $.confirm({
                 title: 'Exito',
-                html: json.success,
-                showConfirmButton: false,
-                timer: 1500
-            }).then(function() {
-                l.ladda('stop');
-                if (json.origin == 'update') {
-                    window.location = json.url;
+                content: json.success,
+                type: 'green',
+                typeAnimated: 'true',
+                icon: 'fa fa-check',
+                autoClose: 'close|1500',
+                buttons: {
+                    close: {
+                        isHidden: true,
+                    },
+                },
+                onclose: function () {
+                    l.ladda('stop');
+                    if (json.origin == 'update') {
+                        window.location = json.url;
+                    }
                 }
             });
         },
-        error: function(json) {
+        error: function (json) {
             var jsonString = json.responseJSON;
             if (json.status === 422) {
                 messages_validation(null, false);
-                swal({
-                    type: 'error',
+                // swal({
+                //     type: 'error',
+                //     title: 'Lo siento',
+                //     html: 'Se han encontrado <b>errores</b>, favor de verificar los datos.',
+                //     showConfirmButton: false,
+                //     timer: 2000
+                // });
+                $.confirm({
                     title: 'Lo siento',
-                    html: 'Se han encontrado <b>errores</b>, favor de verificar los datos.',
-                    showConfirmButton: false,
-                    timer: 2000
+                    type: 'red',
+                    content: 'Se han encontrado <b>errores</b>, favor de verificar los datos.',
+                    typeAnimated: true,
+                    icon: 'fa fa-times',
+                    autoClose: 'close|2000',
+                    buttons: {
+                        close: {
+                            isHidden: true
+                        }
+                    }
                 });
                 var errors = jsonString.errors;
                 messages_validation(errors, true);
             }
             if (json.status === 409) {
                 var errors = jsonString.errors;
-                swal({
-                    type: 'error',
-                    title: 'Error!',
-                    html: errors
+                // swal({
+                //     type: 'error',
+                //     title: 'Error!',
+                //     html: errors
+                // });
+                $.confirm({
+                    title: 'error',
+                    type: 'red',
+                    content: errors
                 });
             }
             l.ladda('stop');
         }
     });
 });
-$('#myformdelete').on('submit', function(e) {
+$('#myformdelete').on('submit', function (e) {
     var el = $('#myformdelete');
     e.preventDefault();
     $.ajax({
         type: "DELETE",
         url: el.attr('action'),
         data: $(this).serialize(),
-        success: function(json) {
-            swal({
-                type: 'success',
+        success: function (json) {
+            // swal({
+            //     type: 'success',
+            //     title: 'Exito',
+            //     html: json.success,
+            //     showConfirmButton: false,
+            //     timer: 1500
+            // }).then(function () {
+            //     window.location = json.url;
+            // });
+            $.confirm({
                 title: 'Exito',
-                html: json.success,
-                showConfirmButton: false,
-                timer: 1500
-            }).then(function() {
-                window.location = json.url;
-            });
+                type: 'green',
+                content: json.success,
+                icon: 'fa fa-check',
+                autoClose: 'close|1500',
+                buttons: {
+                    close: {
+                        isHidden: true
+                    }
+                },
+                onclose: function () {
+                    window.location = json.url;
+                }
+            })
         },
-        error: function(json) {
+        error: function (json) {
             if (json.status === 422) {
                 var jsonString = json.responseJSON;
                 var errors = jsonString.errors;
@@ -277,7 +386,7 @@ $('#myformdelete').on('submit', function(e) {
         }
     });
 });
-$('#myformdeletei').on('submit', function(e) {
+$('#myformdeletei').on('submit', function (e) {
     var el = $('#myformdeletei');
     alert(2);
     e.preventDefault();
@@ -285,26 +394,44 @@ $('#myformdeletei').on('submit', function(e) {
         type: "DELETE",
         url: el.attr('action'),
         data: $(this).serialize(),
-        success: function(json) {
-            swal({
-                icon: 'success',
+        success: function (json) {
+            // swal({
+            //     icon: 'success',
+            //     title: 'Exito',
+            //     content: {
+            //         element: 'p',
+            //         attributes: {
+            //             innerHTML: json.success,
+            //         },
+            //     },
+            //     showConfirmButton: false,
+            //     timer: 1500
+            // }).then(function () {
+            //     var url = $('.myformdeletei').attr('action');
+            //     url = url.replace(/\/[^\/]*$/, '/0');
+            //     $('.myformdeletei').attr('action', url);
+            //     $('.dt_default').DataTable().ajax.reload();
+            // });
+            $.confirm({
                 title: 'Exito',
-                content: {
-                    element: 'p',
-                    attributes: {
-                        innerHTML: json.success,
-                    },
+                type: 'green',
+                content: json.success,
+                autoClose: 'close|1500',
+                icon: 'fa fa-check',
+                buttons: {
+                    close: {
+                        isHidden: true
+                    }
                 },
-                showConfirmButton: false,
-                timer: 1500
-            }).then(function() {
-                var url = $('.myformdeletei').attr('action');
-                url = url.replace(/\/[^\/]*$/, '/0');
-                $('.myformdeletei').attr('action', url);
-                $('.dt_default').DataTable().ajax.reload();
+                onclose: function () {
+                    var url = $('.myformdeletei').attr('action');
+                    url = url.replace(/\/[^\/]*$/, '/0');
+                    $('.myformdeletei').attr('action', url);
+                    $('.dt_default').DataTable().ajax.reload();
+                }
             });
         },
-        error: function(json) {
+        error: function (json) {
             if (json.status === 422) {
                 var jsonString = json.responseJSON;
                 var errors = jsonString.errors;
@@ -317,7 +444,7 @@ $('#myformdeletei').on('submit', function(e) {
 
 function messages_validation(fields, show) {
     if (show == true) {
-        $.each(fields, function(key, value) {
+        $.each(fields, function (key, value) {
             $('#el-' + key).html(value);
             $('#' + key).addClass('is-invalid');
         });
@@ -329,31 +456,55 @@ function messages_validation(fields, show) {
 }
 
 function destroy(id) {
-    swal({
+    // swal({
+    //     title: '¡ Advertencia !',
+    //     text: "Esta seguro de eliminar los datos?",
+    //     icon: "warning",
+    //     buttons: {
+    //         cancel: {
+    //             text: 'Cancelar',
+    //             value: false,
+    //             visible: true,
+    //             className: 'btn btn-default',
+    //             closeModal: true,
+    //         },
+    //         confirm: {
+    //             text: 'Confirmar',
+    //             value: true,
+    //             visible: true,
+    //             className: 'btn btn-primary',
+    //             closeModal: true
+    //         }
+    //     }
+    // }).then((result) => {
+    //     if (result) {
+    //         var url = $('.myformdeletei').attr('action');
+    //         url = url.replace(/\/[^\/]*$/, '/' + id)
+    //         $('.myformdeletei').attr('action', url).submit();
+    //     }
+    // });
+    $.confirm({
         title: '¡ Advertencia !',
-        text: "Esta seguro de eliminar los datos?",
-        icon: "warning",
+        content: "¿Está seguro de eliminar los datos?",
+        type: 'orange',
+        typeAnimated: 'true',
+        icon: 'fa fa-warning',
         buttons: {
-            cancel: {
-                text: 'Cancelar',
-                value: false,
-                visible: true,
-                className: 'btn btn-default',
-                closeModal: true,
-            },
-            confirm: {
+            confirmar: {
                 text: 'Confirmar',
-                value: true,
-                visible: true,
-                className: 'btn btn-primary',
-                closeModal: true
+                btnClass: 'btn btn-primary',
+                action: function () {
+                    var url = $('.myformdeletei').attr('action');
+                    url = url.replace(/\/[^\/]*$/, '/' + id)
+                    $('.myformdeletei').attr('action', url).submit();
+                }
+            },
+            cancelar: {
+                text: 'Cancelar',
+                btnClass: 'btn btn-default',
+                action: function () {
+                }
             }
-        }
-    }).then((result) => {
-        if (result) {
-            var url = $('.myformdeletei').attr('action');
-            url = url.replace(/\/[^\/]*$/, '/' + id)
-            $('.myformdeletei').attr('action', url).submit();
         }
     });
 }
